7ce81e594ca2e749a1395b6e3ec16e0cf083577c
Guillermo Cebollero <guillermocebollero@gmail.com>
2017-02-10 14:55:55 +0100

    Code optimization

--- abilities/index.php
+++ abilities/index.php
@@ -17,13 +17,13 @@
     <div class="nav-wrapper container">
       <a id="logo-container" href="#" class="brand-logo">CV Online</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="../../resources/Guillermo Cebollero CV-ES.pdf"><i class="material-icons left">file_download</i>Descargar en PDF</a></li>
+        <li><a href="/resources/Guillermo Cebollero CV-ES.pdf"><i class="material-icons left">file_download</i>Descargar en PDF</a></li>
         <li><a href="http://www.guillermocebollero.es/redirect.php"><i class="material-icons left">translate</i> Change to English version</a></li>
       </ul>
       <ul id="nav-mobile" class="side-nav show-on-medium-and-down">
         <li><div class="userView">
           <div class="background">
-            <img src="../images/wood-background.jpg">
+            <img src="/cv/images/wood-background.jpg">
           </div>
           <a href="#!user"><img class="circle" src="../images/profilePic.jpg"></a>
           <a href="#!name"><span class="white-text name">Guillermo Cebollero</span></a>
@@ -54,7 +54,7 @@
         </div>
       </div>
     </div>
-    <div class="parallax"><img src="../images/background2.jpg" alt="Unsplashed background img 2"></div>
+    <div class="parallax"><img src="/cv/images/background2.jpg" alt="Unsplashed background img 2"></div>
   </div>
   <div class="container">
     <br><br>
@@ -63,111 +63,89 @@
     </div>
     <div class="section">
       <!--   Icon Section   -->
-
+      <div class="row">
       <?php
       $jsonfile = file_get_contents('./abilities.json');
       $json = json_decode($jsonfile);
       $i = 0;
       foreach ($json as $ability) {
-        if ($i%3==0){
-          echo '<div class="row">',"\n";
+        ?>
+        <div class="col s12 m4">
+          <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="<?echo $ability->imageURI;?>">
+            </div>
+            <div class="card-content">
+              <span class="card-title grey-text text-darken-4">
+                <? echo $ability->name_ES;?>
+                <i class="material-icons right">code</i></span>
+                <ul class="pagination center">
+                  <li <? if ($ability->value>0){ echo 'class="active indigo"';}?>><a>1</a></li>
+                  <li <? if ($ability->value>1){ echo 'class="active indigo"';}?>><a>2</a></li>
+                  <li <? if ($ability->value>2){ echo 'class="active indigo"';}?>><a>3</a></li>
+                  <li <? if ($ability->value>3){ echo 'class="active indigo"';}?>><a>4</a></li>
+                  <li <? if ($ability->value>4){ echo 'class="active indigo"';}?>><a>5</a></li>
+                </ul>
+              </div>
+              <div class="card-action">
+                <a href="javaScript:void(0);" class="activator">Mas info</a>
+              </div>
+              <div class="card-reveal center">
+                <span class="card-title grey-text text-darken-4">C++<i class="material-icons right">close</i></span>
+                <p class="flow-text">Experiencia en proyecto pequeños y medios. Uso de estructuras de datos y manejo de ficheros.</p>
+              </div>
+            </div>
+          </div>
+          <?
         }
-        echo '<div class="col s12 m4"><div class="card">';
-        echo '<div class="card-image waves-effect waves-block waves-light">';
-        echo '<img class="activator" src="../images/java-logo.jpg">';
-        echo '</div>';
-        echo '<div class="card-content">';
-        echo '<span class="card-title grey-text text-darken-4">';
-        echo $ability->name_ES;
-        echo '<i class="material-icons right">code</i></span>';
-        echo '<ul class="pagination center">';
-        echo '<li class="';
-        if($ability->value>0){
-          echo 'active indigo';
-        }
-        echo '"><a>1</a></li>';
-        echo '<li class="';
-        if($ability->value>1){
-          echo 'active indigo';
-        }
-        echo '"><a>2</a></li>';
-        echo '<li class="';
-        if($ability->value>2){
-        echo 'active indigo';
-        }
-        echo '"><a>3</a></li>';
-        echo '<li class="';
-        if($ability->value>3){
-        echo 'active indigo';
-        }
-        echo '"><a>4</a></li>';
-        echo '<li class="';
-        if($ability->value>4){
-          echo 'active indigo';
-        }
-        echo '"><a>5</a></li>';
-        echo '</ul>';
-        echo '</div>';
-        echo '<div class="card-action">';
-        echo '<a href="javaScript:void(0);" class="activator">Mas info</a>';
-        echo '</div>';
-        echo '<div class="card-reveal center">';
-        echo '<span class="card-title grey-text text-darken-4">';
-        echo $ability->name_ES;
-        echo '<i class="material-icons right">close</i></span>';
-        echo '<p class="flow-text">';
-        echo $ability->text_ES;
-        echo '</div>';
-        echo '</div>';
-        echo '</div>',"\n";
-        $i=$i+1;
-        if($i%3==0){
-          echo '</div>';
-        }
-        echo '<!--Fin loop -->';
-      }
-      if($i%3!=0){
-        echo '</div>';
-      }
-      ?>
-      <br><br>
-    </div>
-  </div>
-
-    <div class="footer-copyright">
-    <div class="container">
-      Última modificación: <span id="footerLastModified">DATE</span>
-      <span id="cookies" class="right"><a href="#" data-toggle="modal" data-target="modalCookies" class="orange-text bold"><h6>Política de cookies</h6></a></span>
-      <div id="modalCookies" class="modal">
-        <div class="modal-content black-text">
-          <h4>Política de cookies</h4>
-          <p class="flow-text">Cookie es un fichero que se descarga en su ordenador al acceder a determinadas páginas web. Las cookies permiten a una página web, entre otras cosas, almacenar y recuperar información sobre los hábitos de navegación de un usuario o de su equipo y, dependiendo de la información que contengan y de la forma en que utilice su equipo, pueden utilizarse para reconocer al usuario.. El navegador del usuario memoriza cookies en el disco duro solamente durante la sesión actual ocupando un espacio de memoria mínimo y no perjudicando al ordenador. Las cookies no contienen ninguna clase de información personal específica, y la mayoría de las mismas se borran del disco duro al finalizar la sesión de navegador (las denominadas cookies de sesión).</p>
-          <br>
-          <p class="flow-text">La mayoría de los navegadores aceptan como estándar a las cookies y, con independencia de las mismas, permiten o impiden en los ajustes de seguridad las cookies temporales o memorizadas.</p>
-          <br>
-          <h4>¿Qué tipos de cookies utiliza esta página web?</h4>
-          <p class="flow-text"> Cookies técnicas: Son aquéllas que permiten al usuario la navegación a través de una página web, plataforma o aplicación y la utilización de las diferentes opciones o servicios que en ella existan como, por ejemplo, controlar el tráfico y la comunicación de datos, identificar la sesión, acceder a partes de acceso restringido, recordar los elementos que integran un pedido, realizar el proceso de compra de un pedido, realizar la solicitud de inscripción o participación en un evento, utilizar elementos de seguridad durante la navegación, almacenar contenidos para la difusión de videos o sonido o compartir contenidos a través de redes sociales.</p>
-          <br>
-          <p class="flow-text"> Cookies de personalización: Son aquéllas que permiten al usuario acceder al servicio con algunas características de carácter general predefinidas en función de una serie de criterios en el terminal del usuario como por ejemplo serian el idioma, el tipo de navegador a través del cual accede al servicio, la configuración regional desde donde accede al servicio, etc.</p>
-          <br>
-          <p class="flow-text"> Cookies de análisis: Son aquéllas que bien tratadas por nosotros o por terceros, nos permiten cuantificar el número de usuarios y así realizar la medición y análisis estadístico de la utilización que hacen los usuarios del servicio ofertado. Para ello se analiza su navegación en nuestra página web con el fin de mejorar la oferta de productos o servicios que le ofrecemos.</p>
-          <br>
-          <p class="flow-text"> Cookies de terceros: La Web de guillermocebollero.es puede utilizar servicios de terceros que, por cuenta de guillermocebollero.es, recopilaran información con fines estadísticos, de uso del Site por parte del usuario y para la prestacion de otros servicios relacionados con la actividad del Website y otros servicios de Internet. </p>
-        </div>
-        <div class="modal-footer">
-          <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Aceptar</a>
-        </div>
+        ?>
+        <br><br>
       </div>
     </div>
   </div>
-</footer>
-<!--  Scripts-->
-<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="/cv/js/materialize.min.js"></script>
-<script src="/cv/js/init.js"></script>
-<script src="/cv/js/widget.js"></script>
-<script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js"></script>
-<script src='https://www.google.com/recaptcha/api.js'></script>
+  <footer class="page-footer teal">
+    <div class="container">
+      <div class="row">
+        <div class="col s12">
+          <h5 class="white-text">Agradecimientos</h5>
+          <h6 class="white-text"> Página web desarrollada con <a class="orange-text" href="http://www.materializecss.com">MaterializeCSS</a> y fotos de <a class="orange-text" href="http://www.freepik.es/onlyyouqj">@onlyyouqj</a></h6>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        Última modificación: <span id="footerLastModified">DATE</span>
+        <span id="cookies" class="right"><a href="#" data-toggle="modal" data-target="modalCookies" class="orange-text bold"><h6>Política de cookies</h6></a></span>
+        <div id="modalCookies" class="modal">
+          <div class="modal-content black-text">
+            <h4>Política de cookies</h4>
+            <p class="flow-text">Cookie es un fichero que se descarga en su ordenador al acceder a determinadas páginas web. Las cookies permiten a una página web, entre otras cosas, almacenar y recuperar información sobre los hábitos de navegación de un usuario o de su equipo y, dependiendo de la información que contengan y de la forma en que utilice su equipo, pueden utilizarse para reconocer al usuario.. El navegador del usuario memoriza cookies en el disco duro solamente durante la sesión actual ocupando un espacio de memoria mínimo y no perjudicando al ordenador. Las cookies no contienen ninguna clase de información personal específica, y la mayoría de las mismas se borran del disco duro al finalizar la sesión de navegador (las denominadas cookies de sesión).</p>
+            <br>
+            <p class="flow-text">La mayoría de los navegadores aceptan como estándar a las cookies y, con independencia de las mismas, permiten o impiden en los ajustes de seguridad las cookies temporales o memorizadas.</p>
+            <br>
+            <h4>¿Qué tipos de cookies utiliza esta página web?</h4>
+            <p class="flow-text"> Cookies técnicas: Son aquéllas que permiten al usuario la navegación a través de una página web, plataforma o aplicación y la utilización de las diferentes opciones o servicios que en ella existan como, por ejemplo, controlar el tráfico y la comunicación de datos, identificar la sesión, acceder a partes de acceso restringido, recordar los elementos que integran un pedido, realizar el proceso de compra de un pedido, realizar la solicitud de inscripción o participación en un evento, utilizar elementos de seguridad durante la navegación, almacenar contenidos para la difusión de videos o sonido o compartir contenidos a través de redes sociales.</p>
+            <br>
+            <p class="flow-text"> Cookies de personalización: Son aquéllas que permiten al usuario acceder al servicio con algunas características de carácter general predefinidas en función de una serie de criterios en el terminal del usuario como por ejemplo serian el idioma, el tipo de navegador a través del cual accede al servicio, la configuración regional desde donde accede al servicio, etc.</p>
+            <br>
+            <p class="flow-text"> Cookies de análisis: Son aquéllas que bien tratadas por nosotros o por terceros, nos permiten cuantificar el número de usuarios y así realizar la medición y análisis estadístico de la utilización que hacen los usuarios del servicio ofertado. Para ello se analiza su navegación en nuestra página web con el fin de mejorar la oferta de productos o servicios que le ofrecemos.</p>
+            <br>
+            <p class="flow-text"> Cookies de terceros: La Web de guillermocebollero.es puede utilizar servicios de terceros que, por cuenta de guillermocebollero.es, recopilaran información con fines estadísticos, de uso del Site por parte del usuario y para la prestacion de otros servicios relacionados con la actividad del Website y otros servicios de Internet. </p>
+          </div>
+          <div class="modal-footer">
+            <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Aceptar</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="/cv/js/materialize.min.js"></script>
+  <script src="/cv/js/init.js"></script>
+  <script src="/cv/js/widget.js"></script>
+  <script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+  <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js"></script>
+  <script src='https://www.google.com/recaptcha/api.js'></script>
 </body>
 </html>
